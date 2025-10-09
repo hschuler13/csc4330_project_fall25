d558f3fa1134a814a1b61b2817ba8d2b11b91229
agill32 <ambergill2004@gmail.com>
2025-10-09 12:05:42 -0500

    First part of login page creation

---
+++ src/AppAG.tsx
@@ -0,0 +1,12 @@
+import "./AppAG.css";
+import LoginPage from "./Pages-AG/LoginPage";
+import AfterLogin from "./Pages-AG/AfterLogin";
+
+
+function App() {
+  return (
+    <LoginPage/>
+  ) 
+}
+
+export default App
---
+++ src/Pages-AG/AfterLogin.tsx
@@ -0,0 +1,8 @@
+
+function AfterLogin() {
+  return (
+    <div className = "h-screen w-screen flex flex-col bg-linear-to-r from-blue-950 to-purple-950"/>
+  ) 
+}
+
+export default AfterLogin
